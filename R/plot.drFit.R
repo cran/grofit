3855cc4ea41c2e9bb88485835eb6541f9488b37b
Unknown author <unknown@unknown>
2012-08-05 00:00:00 +0000

    version 1.1.1

--- R/plot.drFit.R
+++ R/plot.drFit.R
@@ -7,7 +7,7 @@ n <- length(x$drFittedSplines)
 
 # /// plot all drFitSpline objects
 for (i in 1:n){
-x11()
+#x11()
 try(plot(x$drFittedSplines[[i]]))
 title(as.character(x$drFittedSplines[[i]]$drID))
 }
